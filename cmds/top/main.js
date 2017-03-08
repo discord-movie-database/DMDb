92268b08b79a57e9d18cc3058c8c4fda4db2343e
DumplingsWithToads <wavewaffles0@gmail.com>
2017-03-08 23:52:02 +0000

    Fixed top

--- cmds/top/main.js
+++ cmds/top/main.js
@@ -1,25 +1,21 @@
 const fs = require('fs');
 const u = require('../../util/main.js');
 
-const pageResults = async (top, page, callback) => {
-    callback(top.splice(page * 10, 10));
-}
-
 const c = module.exports = {};
 c.settings = require('./settings.json');
 c.process = async (bot, msg, cmdArgs, guild, user) => {
     let message = await bot.createMessage(msg.channel.id, 'ℹ Getting information...');
     let top = require('./top.json');
     let page = (cmdArgs[0] - 1) || 0;
-    let pages = (top.length / 10) || 0;
-    let results = top.slice(page * 10, 10);
+    let pages = [];
+    for (let i = 0; i < top.length; i += 10) pages.push(top.slice(i, i + 10));
+    let results = pages[page];
+    if (!results) return message.edit('❌ Page not found.');
     let response = '';
-    for (let i = 0; i < results.length; i++) {
-        response += `\n[**${results[i].num + 1}**] ${results[i].name} *(${results[i].year})* **${results[i].rating}** *${results[i].id}*`;
-    }
+    for (let i = 0; i < results.length; i++) response += `\n[**${results[i].num + 1}**] ${results[i].name} *(${results[i].year})* **${results[i].rating}** *${results[i].id}*`;
     message.edit({embed: {
         title: 'Top Rated Titles',
-        description: `**Page: ${page + 1}/${pages}**\n${response}`,
+        description: `**Page: ${page + 1}/${pages.length}**\n${response}`,
         color: 0xE6B91E
     }});
 }
