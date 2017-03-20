adcae5b383292b2b2bd771fba9868dddd61bdf91
DumplingsWithToads <wavewaffles0@gmail.com>
2017-03-20 01:24:38 +0000

    More trivia progress

--- cmds/trivia/main.js
+++ cmds/trivia/main.js
@@ -2,14 +2,20 @@ const fs = require('fs');
 const u = require('../../util/main.js');
 const config = require('../../config.json');
 
-const triviaSettings = () => {
-    return JSON.parse(fs.readFileSync('./cmds/trivia/trivia.json'));
+const triviaSettings = (type, update) => {
+    type = type.toLowerCase();
+    if (type === 'get') return JSON.parse(fs.readFileSync('./cmds/trivia/trivia.json'));
+    if (type === 'update') return fs.writeFileSync('./cmds/trivia/trivia.json', JSON.stringify(update, null, 2));
+    return false;
 }
 
 const args = {
     "start": {
-        "process": () => {
-            
+        "process": (bot, msg) => {
+            let aTrivSet = triviaSettings('get');
+            if (aTrivSet.started) return bot.createMessage(msg.channel.id, 'A game of trivia has already been started.');
+            aTrivSet.started = true;
+            let updTrivSet = triviaSettings('update', aTrivSet);
         },
         "restricted": true,
         "description": "Start a game.",
@@ -17,7 +23,10 @@ const args = {
     },
     "end": {
         "process": () => {
-            
+            let aTrivSet = triviaSettings('get');
+            if (!aTrivSet.started) return bot.createMessage(msg.channel.id, 'There are no games of trivia running.');
+            aTrivSet.started = false;
+            let updTrivSet = triviaSettings('update', aTrivSet);
         },
         "restricted": true,
         "description": "Force end the current game.",
@@ -55,8 +64,8 @@ c.process = async (bot, msg, cmdArgs, guild, user) => {
     if (msg.channel.guild && msg.channel.guild.id !== "223563882642407426") return;
     cmdArgs = cmdArgs.join('').toLowerCase().split(' ');
     if (cmdArgs[0] && args[cmdArgs[0]]) {
-        if (args[cmdArgs].restricted && msg.author.id !== msg.channel.guild.ownerID) return;
-        bot.createMessage(msg.channel.id, "Hello");
+        if (args[cmdArgs[0]].restricted && msg.author.id !== msg.channel.guild.ownerID) return;
+        args[cmdArgs[0]].process(bot, msg);
         return;
     }
     let noArgs = '__**Trivia Arguments**__\n';
