fbc7389304d1f97c55b475ac400a016f4830d4d3
Dumplings <send-help@im-stuck-in.space>
2018-02-13 12:42:30 +0000

    Update reload command.

--- cmds/reload.js
+++ cmds/reload.js
@@ -7,6 +7,18 @@ c.settings = {
 };
 
 c.process = async (bot, msg, cmdArgs, guild, user, config, u) => {
+    if (cmdArgs[0]) {
+        const cmd = cmdArgs[0].toLowerCase();
+
+        if (main.commands[cmd]) {
+            u.loader.reloadCommand(cmd);
+
+            return bot.createMessage(msg.channel.id, `✅ Reloaded the command \`${cmd}\`.`);
+        } else {
+            return bot.createMessage(msg.channel.id, '❌ Command doesn\'t exist');
+        }
+    }
+
     u.loader.reloadCommands();
 
     bot.createMessage(msg.channel.id, '✅ Reloaded commands and utils.');
