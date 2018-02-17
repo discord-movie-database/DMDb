af4419d6b8218a9f2861d623b33f969c82dfd9fb
Dumplings <send-help@im-stuck-in.space>
2018-02-17 17:50:32 +0000

    Improved botlist handler

--- handlers/list.js
+++ handlers/list.js
@@ -8,39 +8,41 @@ e.post.all = async (bot) => {
     const count = bot.guilds.size;
 
     await e.post.dbl(count);
-    // await e.post.dbots(count);
+    await e.post.dbots(count);
     // await e.post.carbon(count);
+
+    console.log('Finished posting new server counts.');
 }
 
 e.post.dbl = async (count) => {
-    const dbl = await superagent.post('https://discordbots.org/api/bots/412006490132447249/stats').set({
+    const post = await superagent.post('https://discordbots.org/api/bots/412006490132447249/stats').set({
         'Authorization': config.token.botlist.dbl,
         'Content-Type': 'application/json'
     }).send({
         'server_count': count
     }).catch((err) => console.error(err));
 
-    if (api.statuscode !== 200) console.log(`Cannot post new server count to dbl. (${api.statuscode})`);
+    if (post.statuscode !== 200) console.log(`Cannot post new server count to dbl. (${post.statuscode})`);
 }
 
 e.post.dbots = async (count) => {
-    const dbots = await superagent.post('https://bots.discord.pw/api/bots/412006490132447249/stats').set({
+    const post = await superagent.post('https://bots.discord.pw/api/bots/412006490132447249/stats').set({
         'Authorization': config.token.botlist.dbots,
         'Content-Type': 'application/json'
     }).send({
         'server_count': count
     }).catch((err) => console.error(err));
 
-    if (api.statuscode !== 200) console.log(`Cannot post new server count to dbots. (${api.statuscode})`);
+    if (post.statuscode !== 200) console.log(`Cannot post new server count to dbots. (${post.statuscode})`);
 }
 
 e.post.carbon = async (count) => {
-    const carbon = await superagent.post(`https://www.carbonitex.net/discord/data/botdata.php`).set({
+    const post = await superagent.post(`https://www.carbonitex.net/discord/data/botdata.php`).set({
         'Content-Type': 'application/json'
     }).send({
         'key': config.token.botlist.carbon,
         'servercount': count
     }).catch((err) => console.error(err));
 
-    if (api.statuscode !== 200) console.log(`Cannot post new server count to carbon. (${api.statuscode})`);
+    if (post.statuscode !== 200) console.log(`Cannot post new server count to carbon. (${post.statuscode})`);
 }
