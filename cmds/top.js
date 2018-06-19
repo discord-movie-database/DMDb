a085be936f05bd23d9f242d9b35fe67fd5c711be
Dumplings <jolrds24@gmail.com>
2018-06-19 22:42:07 +0100

    Top command: Extended scrape interval, added title position difference.

--- cmds/top.js
+++ cmds/top.js
@@ -13,10 +13,25 @@ c.settings = {
 c.top = require('../top.json');
 c.scrape = async () => {
     const topData = await handlers.scrape.top();
-    if (topData) c.top = topData;
+    if (!topData) return;
+
+    for (let o = 0; o < c.top.titles.length; o++) {
+        for (let n = 0; n < topData.titles.length; n++) {
+            const oTitle = c.top.titles[o];
+            const nTitle = topData.titles[n];
+
+            const oPosition = o;
+            const nPosition = topData.titles.map((e) => e.id).indexOf(oTitle.id);
+
+            if (oTitle.id === nTitle.id)
+                topData.titles[n].difference = oPosition - nPosition;
+        }
+    }
+
+    c.top = topData;
 }
 c.start = () => c.scrape();
-c.interval = setInterval(c.scrape, 86400000);
+c.interval = setInterval(c.scrape, 604800000);
 
 c.reload = () => {
     delete require.cache[require.resolve(`../top.json`)];
@@ -25,7 +40,7 @@ c.reload = () => {
 
 c.process = async (bot, msg, cmdArgs, guild, user, config, u) => {
     const message = await bot.createMessage(msg.channel.id, 'ℹ Getting information...');
-    
+
     const titles = c.top.titles;
     const page = (cmdArgs[0] - 1) || 0;
 
@@ -37,10 +52,14 @@ c.process = async (bot, msg, cmdArgs, guild, user, config, u) => {
 
     let fields = [];
     for (let i = 0; i < results.length; i++) {
+        const direction = results[i].difference > 0 ? '▲'
+                        : results[i].difference < 0 ? '▼'
+                        : '●';
+        const difference = results[i].difference;
 
         fields.push({
             name: `${results[i].name}`,
-            value: `**${results[i].rating}** **|** ${results[i].year} **|** ${results[i].id} **|** ${results[i].index + 1}`,
+            value: `**${results[i].rating}** **|** ${results[i].year} **|** ${results[i].id} **|** Pos: ${results[i].index + 1} Diff: ${difference === 0 ? '' : Math.abs(difference) + ' '}${direction}`,
             inline: false
         });
     }
