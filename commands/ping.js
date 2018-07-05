98b0dd76a2225f8fa200196bbb6f79c8dc3d3fd4
Dumplings <jolrds24@gmail.com>
2018-07-05 21:55:02 +0100

    ping

---
+++ commands/ping.js
@@ -0,0 +1,18 @@
+const Command = require('../handlers/commandHandler');
+
+class PingCommand extends Command {
+    constructor(client) {
+        super(client, {
+            "shortDescription": "PONG",
+            "longDescription": "Get latency between the bot and Discord.",
+            "visible": false,
+            "restricted": false
+        });
+    }
+
+    async process(message) {
+        this.client.createMessage(message.channel.id, 'Pong!');
+    }
+}
+
+module.exports = PingCommand;
