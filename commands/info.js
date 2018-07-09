ed900d5f2209c74e29d860b6c1bb279ec3e3546a
Ray <richja02@gmail.com>
2018-07-09 15:27:36 +0100

    Add info command

---
+++ commands/info.js
@@ -0,0 +1,44 @@
+const Command = require('../handlers/commandHandler');
+
+class InfoCommand extends Command {
+    constructor(client) {
+        super(client, {
+            'shortDescription': 'Information about the bot.',
+            'longDescription': 'TODO',
+            'visible': true,
+            'restricted': false,
+            'weight': -999
+        });
+
+        this.package = require('../package.json');
+    }
+
+    async process(message) {
+        this.client.handlers.embed.create(message.channel.id, {
+            'title': 'DMDb Information',
+            'description': 'Invite Bot: <https://dmdb.me/invite> | Server: <https://dmdb.me/server>',
+            'fields': [{
+                'name': 'Developer',
+                'value': 'Dumplings#7460'
+            }, {
+                'name': 'Library',
+                'value': `Eris ${this.package.dependencies.eris}`
+            }, {
+                'name': 'Shards',
+                'value': `${this.client.shards.size}`
+            }, {
+                'name': 'Guilds',
+                'value': `${this.client.guilds.size}`
+            }, {
+                'name': 'Channels',
+                'value': `${Object.keys(this.client.channelGuildMap).length}`
+            }, {
+                'name': 'Users',
+                'value': `${this.client.users.size}`
+            }].map((field) => ({ ...field, 'inline': true })),
+            'footer': 'Data sourced from The Movie Database'
+        });
+    }
+}
+
+module.exports = InfoCommand;
