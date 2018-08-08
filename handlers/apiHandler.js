a81ce595b819dbcdc08051ba63b42b4caba8dab2
Dumplings <jolrds24@gmail.com>
2018-08-08 05:40:00 +0100

    re-write api handler

--- handlers/apiHandler.js
+++ handlers/apiHandler.js
@@ -1,41 +1,92 @@
-const needle = require('needle');
+const fetch = require('node-fetch');
 
 class APIHandler {
     constructor(client) {
         this.client = client;
 
-        // API Endpoint.
-        this.tmdb = `https://api.themoviedb.org/3/`;
+        // Endpoint base
+        this.base = `https://api.themoviedb.org/3/`;
     }
 
-    async _get(path, queries) {
-        // Format queries.
-        if (queries) queries = Object.keys(queries).map(query =>
-            `&${query}=${queries[query]}`).join('');
+    // Error
+    error(message) {
+        return { 'error': message };
+    }
 
-        // Request.
-        const request = `${this.tmdb}\
-${path}\
-?api_key=${this.client.config.tokens.api.tmdb}\
-${queries ? queries : ''}`;
+    // Check type of ID. IMDb, TMDb, or none
+    ID(query) {
+        const type = query.match(/^t(t)?\d+/);
+        return type ? type[1] ? 'imdb' : 'tmdb' : false;
+    }
 
-        // POST.
-        const { 'body': response } = await needle('get', request)
-            .catch(this.client.handlers.log.error);
+    // Get from the API
+    async get(endpoint, query) {
+        let valid;
+
+        query = query ? query : '';
+        let apiKey = `api_key=${this.client.config.tokens.api.tmdb}&language=en-US`;
+        apiKey = query ? `&${apiKey}` : `?${apiKey}`;
+
+        try {
+            let response = await fetch(`${this.base}${endpoint}${query}${apiKey}`);
+            response = await response.json();
+
+            if (response && typeof response.success === 'undefined')
+                valid = response;
+        } catch (err) {
+            console.log(err);
+        }
+
+        if (!valid) return this.error('Unable to get data from API.');
+        return valid;
+    }
+
+    // Get a movie ID
+    async getMovieID(query) {
+        const ID = this.ID(query);
+
+        if (this.ID === 'tmdb') return query.slice(1);
+
+        if (this.ID === 'imdb') {
+            const movies = this.get(`find/${query}`);
+            if (movies.error) return movies;
+
+            if (!movies.movie_results[0])
+                return this.error('No results found.');
+            const movieID = movies.movie_results[0].id;
+
+            return movieID;
+        }
+
+        const movies = await this.getMovies(query);
+        if (movies.error) return movies;
+
+        return movies[0].id;
+    }
+
+    // Get data for a movie
+    async getMovie(query) {
+        const movieID = await this.getMovieID(query);
+        if (movieID.error) return movieID;
+
+        const movie = await this.get(`movie/${movieID}`);
+        if (movie.error) return movie;
+        if (!movie.id) return this.error('Invalid ID.');
+
+        return movie;
+    }
+
+    // Get multiple movies
+    async getMovies(query, page) {
+        page = page ? page : 1;
+
+        const movies = await this.get(`search/movie`,
+            `?query=${query}&page=${page}&include_adult=true`);
         
-        // Response.
-        if (!response || response.success === false)
-            return { 'error': 'API is having issues.' };
-        return response;
-    }
+        if (movies.error) return movies;
+        if (!movies.results[0]) return this.error('No results found.');
 
-    async search(query, page) {
-        // Get movies from API.
-        return this._get(`search/movie`, {
-            'query': query,
-            'page': page || 1,
-            'include_adult': true
-        });
+        return movies.results;
     }
 }
 
