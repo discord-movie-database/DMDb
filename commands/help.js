27a5d5c17ef3b45e803d0c529a55a95e8e78dc12
Dumplings <jolrds24@gmail.com>
2018-10-01 23:28:34 +0100

    More helpful help command

--- commands/help.js
+++ commands/help.js
@@ -10,12 +10,40 @@ class InfoCommand extends Command {
         });
     }
 
-    async process(message) {
+    commandDescription(message) {
+        // Get & format command name
+        let commandName = message.arguments[0];
+        if (commandName) commandName = commandName.toLowerCase();
+
+        // Check if command exists
+        if (!this.client.commands[commandName])
+            return this.embed.error(message.channel.id, 'Command not found.');
+
+        // Command
+        const command = this.client.commands[commandName];
+
+        // Command information embed
+        this.embed.create(message.channel.id, {
+            'title': `Command: ${commandName.charAt(0).toUpperCase() + commandName.slice(1)}`,
+            'description': command.info.longDescription ||
+                           command.info.shortDescription ||
+                           'Description is currently unavailable.',
+            'fields': [{
+                'name': 'Visible',
+                'value': this.yesno(command.info.visible || false),
+                'inline': true
+            }, {
+                'name': 'Restricted',
+                'value': this.yesno(command.info.restricted || false),
+                'inline': true }] });
+    }
+
+    commandList(message) {
         // Embed template
         const embed = {
             'title': 'Discord Movie Database',
             'description': `Use \`${this.config.prefix}help [Command Name]\` \
-to get more detailed information about a command.\n`,
+        to get more detailed information about a command.\n`,
             'fields': []
         };
 
@@ -31,13 +59,21 @@ to get more detailed information about a command.\n`,
 
             // Append commands to response
             embed.fields.push({
-                'name': `${this.config.prefix}${commandName} <${command.usage}>`,
+                'name': `${this.config.prefix}${commandName} ${command.usage ? `<${command.usage}>` : ''}`,
                 'value': `- ${this.client.commands[commands[i]].info.shortDescription}`
             });
         }
 
         // Response
         this.embed.create(message.channel.id, embed);
+    }
+
+    async process(message) {
+        // Check for command
+        if (message.arguments[0]) return this.commandDescription(message);
+
+        // List all commands
+        this.commandList(message);
     }
 }
 
