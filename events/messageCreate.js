060053842f71c36632e4e7a761cb99fc79e03850
Dumplings <jolrds24@gmail.com>
2018-10-06 17:15:36 +0100

    Improve command execution logging

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -34,7 +34,9 @@ class MsgEvent {
             return this.client.handlers.embed.error(message.channel.id, `Error executing command: ${commandName}`);
         }
 
-        this.client.handlers.log.info(`Command: ${commandName} | User: ${message.author.id} | Chan: ${message.channel.guild ? message.channel.guild.id : 'DM'}${message.arguments[0] ? ` | Args: ${message.arguments.join('')}` : ''}`);
+        this.client.handlers.log.info(`Command: ${commandName} | User: ${message.author.id} | \
+Guild: ${message.channel.guild ? message.channel.guild.id : 'DM'}${message.arguments[0] ? ` | \
+Args: ${message.arguments.join(' ')}` : ''}`);
     }
 }
 
