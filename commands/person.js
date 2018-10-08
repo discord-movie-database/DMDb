ff0ba39f18f2b7964a84fbfc339255c524662ad0
Ray <richja02@gmail.com>
2018-10-08 22:03:37 +0100

    More default field values

--- commands/person.js
+++ commands/person.js
@@ -31,13 +31,13 @@ class ActorCommand extends Command {
             'title': person.name,
             'description': this.description(person.biography),
             'thumbnail': this.thumbnail(person.profile_path),
-            'fields': this.parseEmbedFields([{ 'name': 'Known For', 'value': person.known_for_department },
+            'fields': this.parseEmbedFields([{ 'name': 'Known For', 'value': this.knownForDep(person.known_for_department) },
                 { 'name': 'Birthday', 'value': this.birthday(person.birthday) },
                 { 'name': 'Deathday', 'value': this.deathday(person.deathday) },
                 { 'name': 'Gender', 'value': this.gender(person.gender) },
-                { 'name': 'Place of Birth', 'value': person.place_of_birth },
+                { 'name': 'Place of Birth', 'value': this.placeOfBirth(person.place_of_birth) },
                 { 'name': 'Popularity', 'value': this.popularity(person.popularity) },
-                { 'name': 'IMDb ID', 'value': this.check(person.imdb_id) },
+                { 'name': 'IMDb ID', 'value': this.IMDbID(person.imdb_id) },
                 { 'name': 'TMDb ID', 'value': this.TMDbID(person.id)
             }])
         });
