8f2cbaa5f954370a3eeed154332216e9303b6efa
Dumplings <jolrds24@gmail.com>
2018-11-05 23:16:35 +0000

    update prefix config option

--- commands/config.js
+++ commands/config.js
@@ -9,23 +9,43 @@ class ConfigCommand extends Command {
             'restricted': false
         });
 
+        this.dbHandler = this.client.handlers.db;
+
         this.configOptions = {
             'prefix': {
                 'description': 'Change the prefix to a number, letter or symbol. Example: \`!?\`',
                 'usage': '<new prefix>',
-                'process': this._optionPrefix
+                'process': this._optionPrefix.bind(this)
+            },
+            'disableCommand': {
+                'description': 'Disable and hide any command.',
+                'usage': '<command name>',
+                'process': this._optionDisableCommand
             }
-            // disabledCommands
         };
+
         this.commandKeys = Object.keys(this.client.commands);
         this.optionKeys = Object.keys(this.configOptions);
     }
 
-    _optionPrefix(message) {
-        // TODO: Update database. 
-        // regex: ^[a-z0-9!"£$%^&*()_+=\-]{1,16}$
-        //
-        // update messageCreate event
+    async _optionPrefix(message) {
+        const prefix = message.arguments[0];
+        if (!prefix) return this.embed.error(message.channel.id, 'New prefix required.');
+
+        const regex = /^[a-z0-9!"£$%^&*()_+=~#~@\';:.,<>?{}`|[\]\/\-]{1,16}$/i;
+        if (!regex.test(prefix)) 
+            return this.embed.error(message.channel.id, `Invalid prefix. Regex: ${regex}`);
+
+        const updatedGuild = await this.dbHandler.updateGuild(message.channel.guild.id, {
+            'prefix': prefix });
+
+        if (updatedGuild)
+            return this.embed.success(message.channel.id, `Updated prefix to ${prefix}`);
+        this.embed.error(message.channel.id, 'Unable to update prefix.');
+    }
+
+    _optionDisableCommand(message) {
+        
     }
 
     optionList(message) {
@@ -33,19 +53,25 @@ class ConfigCommand extends Command {
             'title': 'Guild Configuration',
             'description': 'Customise the bot for this guild.',
 
-            'fields': this.optionKeys.map((key) => ({
-                'name': this.capitaliseStart(key),
+            'fields': this.optionKeys.map((key) => {
+                const option = this.configOptions[key];
 
-                'value': this.configOptions[key].description +
-                    `\nUsage: \`!?config prefix ${this.configOptions[key].usage}\``
-            }))
+                return {
+                    'name': this.capitaliseStart(key),
+                    'value': option.description +
+                    `\nUsage: \`!?config ${key} ${option.usage}\``
+                }
+            })
         });
     }
 
     async process(message) {
+        if (!message.channel.guild)
+            return this.embed.error(message.channel.id, 'This is not a guild.');
+
         // Check if author has permission
         if (!message.member.permission.has('manageGuild')) 
-            return this.embed.error('You do not have the Manage Guild Permission.');
+            return this.embed.error(message.channel.id, 'You do not have the Manage Guild Permission.');
 
         // Check type of argument
         const optionName = message.arguments[0];
@@ -53,8 +79,9 @@ class ConfigCommand extends Command {
 
         // Check if option exists
         if (!this.configOptions[optionName])
-            return this.embed.error(`Option not found.`);
+            return this.embed.error(message.channel.id, 'Option not found.');
         
+        message.arguments = message.arguments.slice(1);
         // Run option function
         this.configOptions[optionName].process(message);
     }
