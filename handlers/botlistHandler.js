42895f213054f826e2b198781d538fed2194a7f1
Ray <richja02@gmail.com>
2018-12-22 23:49:59 +0000

    Add bots.ondiscord.xyz

--- handlers/botlistHandler.js
+++ handlers/botlistHandler.js
@@ -14,13 +14,16 @@ class ListHandler {
     }
 
     updateAllSites() {
-        this.discordbotsOrg();
-        this.discordbotsGg();
-    }
-
-    async discordbotsOrg() {
         const guildCount = this.client.guilds.size;
 
+        this.client.handlers.log.info(`Posting ${guildCount} guilds...`);
+
+        this.discordbotsOrg(guildCount);
+        this.discordbotsGg(guildCount);
+        this.botsOndiscordXyz(guildCount);
+    }
+
+    async discordbotsOrg(guildCount) {
         const endpointPrefix = 'https://discordbots.org/api';
         const endpointURL = `${endpointPrefix}/bots/${this.clientID}/stats`;
         
@@ -29,15 +32,14 @@ class ListHandler {
             apiResponse = await request.post(endpointURL)
                 .set('Authorization', this.client.config.tokens.botlist.discordbotsOrg)
                 .send({ 'server_count': guildCount });
+
         } catch (err) { return console.log(err); }
         if (!apiResponse) return;
 
-        this.client.handlers.log.success(`Posted ${guildCount} guilds to ${endpointPrefix}`);
+        this._success(endpointPrefix);
     }
 
-    async discordbotsGg() {
-        const guildCount = this.client.guilds.size;
-
+    async discordbotsGg(guildCount) {
         const endpointPrefix = 'https://discord.bots.gg/api/v1';
         const endpointURL = `${endpointPrefix}/bots/${this.clientID}/stats`;
 
@@ -46,10 +48,31 @@ class ListHandler {
             apiResponse = await request.post(endpointURL)
                 .set('Authorization', this.client.config.tokens.botlist.discordBotsGg)
                 .send({ 'guildCount': guildCount });
+
         } catch (err) { return console.log(err); }
         if (!apiResponse) return;
 
-        this.client.handlers.log.success(`Posted ${guildCount} guilds to ${endpointPrefix}`);
+        this._success(endpointPrefix);
+    }
+
+    async botsOndiscordXyz(guildCount) {
+        const endpointPrefix = 'https://bots.ondiscord.xyz/bot-api';
+        const endpointURL = `${endpointPrefix}/bots/${this.clientID}/guilds`;
+
+        let apiResponse;
+        try {
+            apiResponse = await request.post(endpointURL)
+                .set('Authorization', this.client.config.tokens.botlist.botsOndiscordXyz)
+                .send({ 'guildCount': guildCount });
+
+        } catch (err) { return console.log(err); }
+        if (!apiResponse) return;
+
+        this._success(endpointPrefix);
+    }
+
+    _success(endpoint) {
+        this.client.handlers.log.success(`Posted guilds to ${endpoint}`);
     }
 }
 
