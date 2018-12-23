ad73c6a900c42c2f043e6f4de6433a4d7eb70eb9
Ray <richja02@gmail.com>
2018-12-23 00:49:23 +0000

    reset prefix

--- commands/config/optionPrefix.js
+++ commands/config/optionPrefix.js
@@ -5,23 +5,27 @@ class optionPrefix extends template {
         super(client, {
             'name': 'prefix',
             'description': 'Change the prefix to a number, letter or symbol.',
-            'usage': '<new prefix>'
+            'usage': '<new prefix or "reset">'
         });
     }
 
     async process(message) {
-        const prefix = message.arguments[0];
+        let prefix = message.arguments[0];
         if (!prefix) return this.embed.error(message.channel.id, 'New prefix required.');
 
+        const reset = prefix === 'reset' ||
+            prefix === this.client.prefix ? true : false;
+        if (reset) prefix = undefined;
+
         const regex = /^[a-z0-9!"£$%^&*()_+=~#~@\';:.,<>?{}`|[\]\/\-]{1,16}$/i;
-        if (!regex.test(prefix)) 
+        if (!reset && !regex.test(prefix)) 
             return this.embed.error(message.channel.id, `Invalid prefix. Regex: \`${regex}\``);
 
         const updatedGuild = await this.dbHandler.getOrUpdateGuild(message.channel.guild.id, {
             'prefix': prefix });
 
-        if (updatedGuild)
-            return this.embed.success(message.channel.id, `Updated prefix to \`${prefix}\``);
+        if (updatedGuild) return this.embed.success(message.channel.id,
+            `Updated prefix to \`${prefix || this.client.prefix}\``);
         this.embed.error(message.channel.id, 'Unable to update prefix.');
     }
 }
