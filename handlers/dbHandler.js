7c302fc158bdde40cfa29310cbac000d7f9dbb96
Dumplings <jolrds24@gmail.com>
2018-12-23 00:23:06 +0000

    Add usage stat to db & user schema

--- handlers/dbHandler.js
+++ handlers/dbHandler.js
@@ -13,7 +13,14 @@ class DBHandler {
             disabledCommands: [ String ],
             messages: {
                 commanddisabled: { type: Boolean, default: true }
-            }
+            },
+            usageCount: { type: Number, default: 0 }
+        }
+
+        this.userSchema = {
+            id: String,
+            voted: Date,
+            usageCount: { type: Number, default: 0 }
         }
     }
 
@@ -34,18 +41,26 @@ class DBHandler {
 
         this.client.db.connection.on('connected', () =>
             this.client.handlers.log.success('Connected to database.'));
-            
         this.client.db.connection.on('disconnect', () =>
             this.client.handlers.log.warn('Disconnected from database.'));
         
         mongoose.set('useFindAndModify', false);
+
         mongoose.model('guild', this.guildSchema);
+        mongoose.model('user', this.userSchema);
     }
 
-    async getOrUpdateGuild(guildId, updates) {
-        return await mongoose.model('guild').findOneAndUpdate({
-            'id': guildId }, updates || {}, {
-            'upsert': true, 'setDefaultsOnInsert': true, 'new': true });
+    async getOrUpdate(model, ID, updates) {
+        return await mongoose.model(model).findOneAndUpdate({ 'id': ID }, updates || {},
+            { 'upsert': true, 'setDefaultsOnInsert': true, 'new': true });
+    }
+
+    async getOrUpdateGuild(guildID, updates) {
+        return await this.getOrUpdate('guild', guildID, updates);
+    }
+
+    async getOrUpdateUser(userID, updates) {
+        return await this.getOrUpdate('user', userID, updates);
     }
 }
 
