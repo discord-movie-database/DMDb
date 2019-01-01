69da4ce3e818bd8866dbbcf7396ec7b762e02414
Ray <richja02@gmail.com>
2019-01-01 16:22:00 +0000

    Add shards command

---
+++ commands/shards.js
@@ -0,0 +1,24 @@
+const Command = require('../handlers/commandHandler');
+
+class ShardsCommand extends Command {
+    constructor(client) {
+        super(client, {
+            'visible': false,
+            'restricted': false
+        });
+    }
+
+    async process(message) {
+        this.embed.create(message.channel.id, {
+            fields: this.client.shards.map((shard) => ({
+                name: `${shard.id}`,
+                value: `Status: ${this.capitaliseStart(shard.status)} | ` +
+                    `Latency: ${shard.latency}ms | ` +
+                    `Guilds: ${this.client.guilds.filter(guild =>
+                        guild.shard.id === shard.id).length}`
+            }))
+        });
+    }
+}
+
+module.exports = ShardsCommand;
