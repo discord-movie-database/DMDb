af050f46757c1efa5e3737d0d6c350d94c49c03e
Ray <richja02@gmail.com>
2019-01-09 23:22:01 +0000

    get user from db & only store stats in mem

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -10,18 +10,23 @@ class MsgEvent {
     }
 
     async process(message) {
+        message.db = {
+            'guild': {},
+            'user': {}
+        }; // Database Object
+
         if (message.bot) return; // Check if not a bot
 
         if (message.channel.guild) // Get guild information from database
-            message.db = await this.db.getOrUpdateGuild(message.channel.guild.id);
+            message.db.guild = await this.db.getOrUpdateGuild(message.channel.guild.id);
 
         // Get guild prefix
-        if (!message.db.prefix) message.db.prefix = this.client.config.options.bot.prefix;
-        if (!message.content.startsWith(message.db.prefix)) return; // Check prefix
+        if (!message.db.guild.prefix) message.db.guild.prefix = this.client.config.options.bot.prefix;
+        if (!message.content.startsWith(message.db.guild.prefix)) return; // Check prefix
 
         // Command name & arguments
         const messageSplit = message.content.split(' ');
-        const commandName = messageSplit[0].toLowerCase().slice(message.db.prefix.length);
+        const commandName = messageSplit[0].toLowerCase().slice(message.db.guild.prefix.length);
         message.arguments = messageSplit.slice(1);
 
         // Check if command exists
@@ -29,13 +34,16 @@ class MsgEvent {
         const command = this.client.commands[commandName];
 
         // Check if command is disabled in guild
-        if (message.db.disabledCommands && message.db.disabledCommands.indexOf(commandName) > -1)
-            return message.db.messages && message.db.messages.commanddisabled ?
+        if (message.db.guild.disabledCommands && message.db.guild.disabledCommands.indexOf(commandName) > -1)
+            return message.db.guild.messages && message.db.guild.messages.commanddisabled ?
                 this.client.handlers.embed.error(message.channel.id, 'This command is disabled.') : false;
 
         // Check if user has developer permission
         if (command.info.restricted && !this.developers.includes(message.author.id))
             return this.client.handlers.embed.error(message.channel.id, 'No Permission.');
+
+        // Get user information from database
+        message.db.user = await this.db.getOrUpdateUser(message.author.id);
 
         try { // Execute command
             command.process(message);
@@ -45,17 +53,13 @@ class MsgEvent {
                 `There was an error executing this command. Try again later.`);
         }
 
-        // Store stats in memory
+        // Store usage in memory
         this.client.commands[commandName].info.usageCount++;
         this.client.stats.totalUsageCount++;
-        
-        // Store stats in database
-        if (message.db) await this.db.getOrUpdateGuild(message.channel.guild.id, {
-            'usageCount': message.db.usageCount + 1 });
 
         // Command execution log
         this.client.handlers.log.info(`Command: ${commandName} | User: ${message.author.id} | ` +
-            `Guild: ${message.db ? message.channel.guild.id : 'DM'}` +
+            `Guild: ${message.channel.guild ? message.channel.guild.id : 'DM'}` +
             `${message.arguments[0] ? ` | Args: ${message.arguments.join(' ')}` : ''}`);
     }
 }
