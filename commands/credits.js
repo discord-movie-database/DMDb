db4c605b936a0b0b48ffbaab1f737cb96105d3d7
Ray <richja02@gmail.com>
2019-02-09 15:19:54 +0000

    Add credits command

---
+++ commands/credits.js
@@ -0,0 +1,61 @@
+const Command = require('../handlers/commandHandler');
+
+class ShowsCommand extends Command {
+    constructor(client) {
+        super(client, {
+            'shortDescription': 'Get the cast and crew for a movie.',
+            'longDescription': 'TODO',
+            'usage': '<Movie Name or ID>',
+            'weight': 34,
+            'visible': true,
+            'restricted': false
+        });
+    }
+
+    async process(message) {
+        // Check for query
+        if (!message.arguments[0]) return this.usageMessage(message);
+        let query = message.arguments.join(' ');
+
+        // Status of command response
+        const status = await this.searchingMessage(message);
+
+        // Advanced search
+        const flags = this.util.flags(query);
+        query = flags.query;
+
+        const page = (flags.page - 1) || 0;
+
+        // Get credits from API
+        const credits = await this.api.dmdb.getCredits(query);
+        if (credits.error) return this.embed.error(status, credits.error); // Error
+
+        // Put credits into pages
+        const pages = this.util.chunkArray(credits.cast, 7);
+        if (page > pages.length) return this.embed.error(status, 'No Results Found.');
+
+        // Credits at page
+        const cast = pages[page];
+
+        // Response
+        this.embed.edit(status, {
+            'title': credits.title,
+            'description': `Current Page: **${(page + 1)}** **|** ` +
+                `Total Pages: ${pages.length} **|** ` +
+                `Total Results: ${credits.cast.length}`,
+            'thumbnail': this.thumbnail(cast[0].profile_path ||
+                credits.cast[0].profile_path),
+            
+            'fields': cast.map(credit => ({
+                'name': credit.character,
+                'value': `${credit.name} **|** ` +
+                    `${this.gender(credit.gender)} **|** ` +
+                    `${this.TMDbID(credit.id)}`
+            })),
+            
+            'footer': 'TIP: Use the flags (--page) to get more results.'
+        });
+    }
+}
+
+module.exports = ShowsCommand;
