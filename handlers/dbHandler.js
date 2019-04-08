9b381da70cfc979aa4fe4a0b15efb75f296f1572
Ray <richja02@gmail.com>
2019-04-08 02:00:55 +0100

    use schemas from gist

--- handlers/dbHandler.js
+++ handlers/dbHandler.js
@@ -1,23 +1,14 @@
 const mongoose = require('mongoose');
+const schemas = require('dmdb-schemas');
 
 class DBHandler {
     constructor(client) {
         this.client = client;
         this.client.db = mongoose;
 
-        this.guildSchema = {
-            id: String,
-            prefix: { type: String, default: null },
-            disabledCommands: [ String ],
-            messages: {
-                commanddisabled: { type: Boolean, default: true } },
-            anime: { type: Boolean, default: false },
-            tips: { type: Boolean, default: true }
-        }
-        this.userSchema = {
-            id: String,
-            voted: Date
-        }
+        this.guildSchema = schemas.guild;
+        this.userSchema = schemas.user;
+        this.statsSchema = schemas.stats;
     }
 
     async connect() {
@@ -25,7 +16,7 @@ class DBHandler {
             `/${this.client.config.db.name}`;
         const options = this.client.config.db.options;
 
-        await mongoose.connect(url, options).catch(err => {
+        await this.client.db.connect(url, options).catch(err => {
             this.client.handlers.log.error('', err);
             process.exit();
         });
@@ -35,15 +26,19 @@ class DBHandler {
         this.client.db.connection.on('disconnect', () =>
             this.client.handlers.log.warn('Disconnected from database.'));
         
-        mongoose.set('useFindAndModify', false);
+        this.client.db.set('useFindAndModify', false);
 
-        mongoose.model('guild', this.guildSchema);
-        mongoose.model('user', this.userSchema);
+        this.client.db.model('guild', this.guildSchema);
+        this.client.db.model('user', this.userSchema);
+        this.client.db.model('bstats', this.statsSchema);
     }
 
     async getOrUpdate(model, ID, updates) {
-        return await mongoose.model(model).findOneAndUpdate({ 'id': ID }, updates || {},
-            { 'upsert': true, 'setDefaultsOnInsert': true, 'new': true });
+        return await this.client.db.model(model).findOneAndUpdate(
+            { 'id': ID },
+            updates || {},
+            { 'upsert': true, 'setDefaultsOnInsert': true, 'new': true }
+        );
     }
 
     async getOrUpdateGuild(guildID, updates) {
