622eca46b82c0861faec619724d38ee87f3ded78
Dumplings <jolrds24@gmail.com>
2019-06-07 23:40:29 +0100

    Add show flag to trailer command

--- commands/trailer.js
+++ commands/trailer.js
@@ -5,7 +5,7 @@ class TrailerCommand extends Command {
         super(client, {
             'description': 'Get a trailer for a movie.',
             'usage': '<Movie Name or ID>',
-            'flags': false,
+            'flags': ['show'],
             'visible': true,
             'restricted': false,
             'weight': 200
@@ -17,16 +17,19 @@ class TrailerCommand extends Command {
         if (!message.arguments[0]) return this.usageMessage(message);
         let query = message.arguments.join(' ');
 
+        const flags = this.util.flags(query);
+        query = flags.query;
+
         // Status of command response
         const status = await this.searchingMessage(message);
 
         // Get movie from API
-        const trailers = await this.api.dmdb.getTrailers(query);
+        const trailers = flags.show ? await this.api.dmdb.getTVShowTrailers(query) :
+            await this.api.dmdb.getMovieTrailers(query);
         if (trailers.error) return this.embed.error(status, trailers); // Error
 
         // Find trailer
         const trailer = trailers[0];
-        if (!trailer) return this.embed.error(status, 'No trailers found.');
 
         // Response
         this.embed.edit(status, `https://www.youtube.com/watch?v=${trailer.key}`);
