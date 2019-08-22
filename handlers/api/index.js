046244da56c5cc86f064b958115c02a5a80029f9
Ray <richja02@gmail.com>
2019-08-22 19:51:12 +0100

    Remove MAL

--- handlers/api/index.js
+++ handlers/api/index.js
@@ -1,10 +1,8 @@
 const DMDb = require('./dmdb');
-const MyAnimeList = require('./myanimelist');
 
 class APIHandler {
     constructor(client) {
         this.dmdb = new DMDb(client);
-        this.mal = new MyAnimeList(client);
     }
 }
 
