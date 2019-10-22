b9fa01d7b600b2f54e874259b93c4e3724ae1638
Ray <richja02@gmail.com>
2019-10-22 01:01:45 +0100

    Document events.

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,10 +1,25 @@
 import EventStructure from '../structures/event';
 
+/**
+ * Message create event.
+ */
 class MessageCreateEvent extends EventStructure {
+    /**
+     * Create message create event.
+     * 
+     * @param {Object} client DMDb client extends Eris
+     */
     constructor(client) {
         super(client);
     }
 
+    /**
+     * Function to run when event triggers.
+     * 
+     * Handle commands in command handler.
+     * 
+     * @returns {undefined}
+     */
     onEvent(event) {
         this.client.command.onMessageEvent(event);
     }
