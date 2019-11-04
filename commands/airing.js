a187e813f7e07751d4fccf6ba666e3360e56997a
Ray <richja02@gmail.com>
2019-11-04 22:37:55 +0000

    Update airing command

--- commands/airing.js
+++ commands/airing.js
@@ -1,40 +1,57 @@
 import CommandStructure from '../structures/command';
 
+/**
+ * Airing command. Get a list of TV shows that are airing today.
+ */
 class AiringCommand extends CommandStructure {
+    /**
+     * Create airing command.
+     * 
+     * @param {Object} client DMDb client extends Eris
+     */
     constructor(client) {
         super(client, {
             description: 'TV shows that are airing today.',
             usage: false,
             flags: ['page'],
-            visible: true,
             developerOnly: false,
-            weight: 150
+            hideInHelp: false,
+            weight: 150,
         });
     }
 
-    async executeCommand(message) {
-        const query = message.arguments.join(' ');
+    /**
+     * Function to run when command is executed.
+     * 
+     * @param {Object} message Message object
+     * @param {*} commandArguments Command arguments
+     * @param {*} guildSettings Guild settings
+     */
+    async executeCommand(message, commandArguments, guildSettings) {
+        // Status "Searching..." message.
+        const statusMessage = await this.searchingMessage(message);
+        if (!statusMessage) return; // No permission to send messages.
 
-        const status = await this.searchingMessage(message);
+        // Check message for flags.
+        const flags = this.flags.parse(message.content, this.meta.flags);
 
-        const flags = this.flags.parse(query, this.meta.flags);
-
+        // Get airing TV shows from API.
         const airing = await this.tmdb.getTVShowsAiringToday(flags);
-        if (airing.error) return this.embed.error(airing);
+        if (airing.error) return this.embed.error(statusMessage, airing.error);
 
-        this.embed.edit(status, {
-            title: 'TV Shows Airing Today',
-            description: this.resultDescription(airing),
+        // Edit status message with results.
+        this.embed.edit(statusMessage, {
+            title: 'TV Shows Airing Today (EST)',
+            description: this.resultsDescription(airing),
 
-            fields: airing.results.map((show) => ({
-                name: show.name,
-                value: this.joinResult([
-                    `**${show.index}**`,
-                    `First Air Date: ${this.releaseDate(show.first_air_date)}`,
-                    `Vote Average: ${this.voteAverage(show.vote_average)}`,
-                    `${this.TMDbID(show.id)}`
-                ])
-            }))
+            thumbnail: this.thumbnailURL(airing.results[0].poster_path, true),
+
+            // Format results.
+            fields: airing.results.map((show) => this.resultField(show.name, [
+                `Vote Average: ${this.check(show.vote_average)}`,
+                `First Air Date: ${this.date(show.first_air_date)}`,
+                this.TMDbID(show.id)
+            ], show.index)),
         });
     }
 }
