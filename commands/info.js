643530a39ed79c2ccc15c44565654e4944c32407
Ray <richja02@gmail.com>
2019-11-05 19:50:36 +0000

    Update info command

--- commands/info.js
+++ commands/info.js
@@ -1,61 +1,104 @@
 import packageInfo from '../package.json';
-
 import CommandStructure from '../structures/command';
 
+/**
+ * Info command. Get information and stats about the bot.
+ */
 class InfoCommand extends CommandStructure {
+    /**
+     * Create info command.
+     * 
+     * @param {Object} client DMDb client extends Eris
+     */
     constructor(client) {
         super(client, {
             description: 'Information and statistics about the bot.',
             usage: false,
             flags: false,
-            visible: true,
             developerOnly: false,
+            hideInHelp: false,
             weight: 50
         });
+
+        this.stats = this.client.util.getUtil('stats');
     }
 
-    async executeCommand(message) {
-        const stats = this.client.routine.getRoutine('stats');
+    /**
+     * Get how long the bot has been up since last restart.
+     * 
+     * @returns {undefined}
+     */
+    uptime() {
+        const uptime = new Date(this.client.uptime);
 
+        const years = uptime.getUTCFullYear() - 1970;
+        const months = uptime.getUTCMonth();
+        const days = uptime.getUTCDate() - 1;
+        const hours = uptime.getUTCHours();
+        const minutes = uptime.getUTCMinutes();
+        const seconds = uptime.getUTCSeconds();
+
+        let uptimeParsed = '';
+
+        if (years) uptimeParsed += `${years} Year${this.plural(years)}, `;
+        if (months) uptimeParsed += `${months} Month${this.plural(months)}, `;
+        if (days) uptimeParsed += `${days} Day${this.plural(days)}, `;
+        if (hours) uptimeParsed += `${hours} Hour${this.plural(hours)}, `;
+        if (minutes) uptimeParsed += `${minutes} Minute${this.plural(minutes)}, `;
+        if (seconds) uptimeParsed += `${seconds} Second${this.plural(seconds)}`;
+
+        return uptimeParsed;
+    }
+
+    /**
+     * Function to run when command is executed.
+     * 
+     * @param {Object} message Message object
+     * @param {*} commandArguments Command arguments
+     * @param {*} guildSettings Guild settings
+     */
+    async executeCommand(message, commandArguments, guildSettings) {
+        // Create embed.
         this.embed.create(message.channel.id, {
             title: 'DMDb Information',
-            description: this.joinResult([
+            description: this.join([
                 '[Invite Bot](https://bit.ly/2PXWYLR)',
-                '[Support Server](https://discord.gg/fwAxQjV)',
+                '[Support Server]( https://bit.ly/2kYFRPh)',
+                '[Source Code](https://github.com/discord-movie-database/)',
                 '[Website](https://dmdb.xyz)'
-            ], true),
-            fields: [{
+            ]),
+
+            fields: this.fields([{
                 name: 'Bot Version',
-                value: packageInfo.version
+                value: packageInfo.version,
             }, {
                 name: 'Library',
-                value: `Eris ${packageInfo.dependencies.eris}`
+                value: `Eris ${packageInfo.dependencies.eris}`,
             }, {
                 name: 'Node Version',
-                value: `${process.versions.node}`
+                value: `${process.versions.node}`,
             }, {
                 name: 'Guilds',
-                value: `${stats.guilds()}`
+                value: `${this.stats.getGuilds()}`,
             }, {
                 name: 'Channels',
-                value: `${stats.channels()}`
+                value: `${this.stats.getChannels()}`,
             }, {
                 name: 'Users',
-                value: `${stats.users()}`
+                value: `${this.stats.getUsers()}`,
             }, {
                 name: 'Shards',
-                value: `${this.client.shards.size}`
+                value: `${this.client.shards.size}`,
             }, {
                 name: 'Current Shard',
-                value: message.channel.guild ? `${message.channel.guild.shard.id}` : 'N/A'
+                value: `${message.channel.guild.shard.id}`,
             }, {
                 name: 'Commands Executed',
-                name: this.command.commandsExecuted + 1
+                value: this.client.command.commandsExecuted + 1,
             }, {
                 name: 'Uptime',
                 value: this.uptime(),
-            }].map((field) => ({ ...field,
-                inline: typeof field.inline === 'boolean' ? field.inline : true }))
+            }])
         });
     }
 }
