bc4213395afa88241b7f40de596be6d717ac0a4e
cgori <99cgor@gmail.com>
2020-04-09 04:45:30 +0100

    Command aliases

--- src/commands/movie.js
+++ src/commands/movie.js
@@ -1,142 +1,188 @@
-import CommandStructure from '../structures/command';
+import CommandStructure from "../structures/command";
 
 /**
  * Movie command. Get the primary information about a movie.
  */
 class MovieCommand extends CommandStructure {
-    /**
-     * Create movie command.
-     * 
-     * @param {Object} client - DMDb client extends Eris
-     */
-    constructor(client) {
-        super(client, {
-            description: 'Get the primary information about a movie.',
-            usage: '<Query or TMDb/IMDb ID>',
-            flags: ['more', 'year'],
-            developerOnly: false,
-            hideInHelp: false,
-            weight: 700
-        });
-    }
+  /**
+   * Create movie command.
+   *
+   * @param {Object} client - DMDb client extends Eris
+   */
+  constructor(client) {
+    super(client, {
+      description: "Get the primary information about a movie.",
+      usage: "<Query or TMDb/IMDb ID>",
+      flags: ["more", "year"],
+      developerOnly: false,
+      hideInHelp: false,
+      weight: 700,
+      aliases: ["film"],
+    });
+  }
 
-    /**
-     * Function to run when command is executed.
-     * 
-     * @param {Object} message - Message object
-     * @param {Array} commandArguments - Command arguments
-     * @param {Object} guildSettings - Guild settings
-     * @returns {*} A bit of everything...
-     */
-    async executeCommand(message, commandArguments, guildSettings) {
-        // Check for arguments.
-        if (commandArguments.length === 0) return this.usageMessage(message);
+  /**
+   * Function to run when command is executed.
+   *
+   * @param {Object} message - Message object
+   * @param {Array} commandArguments - Command arguments
+   * @param {Object} guildSettings - Guild settings
+   * @returns {*} A bit of everything...
+   */
+  async executeCommand(message, commandArguments, guildSettings) {
+    // Check for arguments.
+    if (commandArguments.length === 0) return this.usageMessage(message);
 
-        // Status "Searching..." message.
-        const statusMessage = await this.searchingMessage(message);
-        if (!statusMessage) return; // No permission to send messages.
+    // Status "Searching..." message.
+    const statusMessage = await this.searchingMessage(message);
+    if (!statusMessage) return; // No permission to send messages.
 
-        // Check message for flags.
-        const flags = this.flags.parse(message.content, this.meta.flags);
-        message.content = flags.query; // Remove flags from query.
+    // Check message for flags.
+    const flags = this.flags.parse(message.content, this.meta.flags);
+    message.content = flags.query; // Remove flags from query.
 
-        // Get API options.
-        const options = this.APIOptions(guildSettings, { year: flags.year });
+    // Get API options.
+    const options = this.APIOptions(guildSettings, { year: flags.year });
 
-        // Get response from API.
-        const response = await this.tmdb.movie.details(message.content, options);
-        if (response.error) return this.embed.error(statusMessage, response.error);
+    // Get response from API.
+    const response = await this.tmdb.movie.details(message.content, options);
+    if (response.error) return this.embed.error(statusMessage, response.error);
 
-        // Edit status message with response data.
-        this.embed.edit(statusMessage, {
-            title: response.title,
-            url: this.TMDbMovieURL(response.id),
+    // Edit status message with response data.
+    this.embed.edit(statusMessage, {
+      title: response.title,
+      url: this.TMDbMovieURL(response.id),
 
-            thumbnail: { url: this.thumbnailURL(response.poster_path) },
-            description: this.description(response.overview),
+      thumbnail: { url: this.thumbnailURL(response.poster_path) },
+      description: this.description(response.overview),
 
-            fields: flags.more ? this.fields([{
-                name: 'Tagline',
-                value: this.check(response.tagline),
-                inline: false,
-            }, {
-                name: 'Status',
-                value: this.check(response.status),
-            }, {
-                name: 'Release Date',
-                value: this.date(response.release_date),
-            }, {
-                name: 'Runtime',
-                value: this.runtime(response.runtime),
-            }, {
-                name: 'Vote Average',
-                value: this.check(response.vote_average),
-            }, {
-                name: 'Vote Count',
-                value: this.number(response.vote_count),
-            }, {
-                name: 'IMDb ID',
-                value: this.check(response.imdb_id),
-            }, {
-                name: `Genre${this.plural(response.genres)}`,
-                value: this.list(response.genres.map((g) => g.name)),
-                inline: false,
-            }, {
-                name: `Spoken Language${this.plural(response.spoken_languages)}`,
-                value: this.list(response.spoken_languages.map((l) => l.name)),
-                inline: false,
-            }, {
-                name: `Production Countr${this.plural(response.production_countries, true)}`,
-                value: this.list(response.production_countries.map((c) => c.name)),
-                inline: false,
-            }, {
-                name: `Production Compan${this.plural(response.production_companies, true)}`,
-                value: this.list(response.production_companies.map((c) => c.name)),
-                inline: false,
-            }, {
-                name: 'Homepage',
-                value: this.check(response.homepage),
-                inline: false,
-            }, {
-                name: 'Budget',
-                value: this.money(response.budget),
-            }, {
-                name: 'Revenue',
-                value: this.money(response.revenue),
-            }, {
-                name: '-',
-                value: '-',
-            }]) : this.fields([response.tagline ? {
-                name: '💬 — Tagline',
-                value: this.check(response.tagline),
-                inline: false,
-            } : { // Show genres instead of tagline if there isn't one.
-                name: `👽 — Genre${this.plural(response.genres)}`,
-                value: this.list(response.genres.map((g) => g.name)),
-                inline: false,
-            }, response.vote_average ? {
-                name: '⭐ — Vote Average',
-                value: `**${this.check(response.vote_average)}** `
-                    + `(${this.number(response.vote_count)} votes)`,
-            } : { // Show status instead of votes if there are none.
-                name: '🗞️ — Status',
-                value: this.check(response.status),
-            }, {
-                name: '📆 — Release Date',
-                value: this.date(response.release_date),
-            }, response.status === 'Released' ? {
-                name: '🎞️ — Runtime',
-                value: this.runtime(response.runtime),
-            } : { // Show language instead of runtime if not released yet.
-                name: '🗣️ — Language',
-                value: this.list(response.spoken_languages.slice(0, 1).map((l) => l.name)),
-            }]),
+      fields: flags.more
+        ? this.fields([
+            {
+              name: "Tagline",
+              value: this.check(response.tagline),
+              inline: false,
+            },
+            {
+              name: "Status",
+              value: this.check(response.status),
+            },
+            {
+              name: "Release Date",
+              value: this.date(response.release_date),
+            },
+            {
+              name: "Runtime",
+              value: this.runtime(response.runtime),
+            },
+            {
+              name: "Vote Average",
+              value: this.check(response.vote_average),
+            },
+            {
+              name: "Vote Count",
+              value: this.number(response.vote_count),
+            },
+            {
+              name: "IMDb ID",
+              value: this.check(response.imdb_id),
+            },
+            {
+              name: `Genre${this.plural(response.genres)}`,
+              value: this.list(response.genres.map((g) => g.name)),
+              inline: false,
+            },
+            {
+              name: `Spoken Language${this.plural(response.spoken_languages)}`,
+              value: this.list(response.spoken_languages.map((l) => l.name)),
+              inline: false,
+            },
+            {
+              name: `Production Countr${this.plural(
+                response.production_countries,
+                true
+              )}`,
+              value: this.list(
+                response.production_countries.map((c) => c.name)
+              ),
+              inline: false,
+            },
+            {
+              name: `Production Compan${this.plural(
+                response.production_companies,
+                true
+              )}`,
+              value: this.list(
+                response.production_companies.map((c) => c.name)
+              ),
+              inline: false,
+            },
+            {
+              name: "Homepage",
+              value: this.check(response.homepage),
+              inline: false,
+            },
+            {
+              name: "Budget",
+              value: this.money(response.budget),
+            },
+            {
+              name: "Revenue",
+              value: this.money(response.revenue),
+            },
+            {
+              name: "-",
+              value: "-",
+            },
+          ])
+        : this.fields([
+            response.tagline
+              ? {
+                  name: "💬 — Tagline",
+                  value: this.check(response.tagline),
+                  inline: false,
+                }
+              : {
+                  // Show genres instead of tagline if there isn't one.
+                  name: `👽 — Genre${this.plural(response.genres)}`,
+                  value: this.list(response.genres.map((g) => g.name)),
+                  inline: false,
+                },
+            response.vote_average
+              ? {
+                  name: "⭐ — Vote Average",
+                  value:
+                    `**${this.check(response.vote_average)}** ` +
+                    `(${this.number(response.vote_count)} votes)`,
+                }
+              : {
+                  // Show status instead of votes if there are none.
+                  name: "🗞️ — Status",
+                  value: this.check(response.status),
+                },
+            {
+              name: "📆 — Release Date",
+              value: this.date(response.release_date),
+            },
+            response.status === "Released"
+              ? {
+                  name: "🎞️ — Runtime",
+                  value: this.runtime(response.runtime),
+                }
+              : {
+                  // Show language instead of runtime if not released yet.
+                  name: "🗣️ — Language",
+                  value: this.list(
+                    response.spoken_languages.slice(0, 1).map((l) => l.name)
+                  ),
+                },
+          ]),
 
-            timestamp: new Date().toISOString(),
+      timestamp: new Date().toISOString(),
 
-            footer: { text: `TMDb ID: ${this.TMDbID(response.id)}` },
-        });
-    }
+      footer: { text: `TMDb ID: ${this.TMDbID(response.id)}` },
+    });
+  }
 }
 
 export default MovieCommand;
--- src/commands/popular.js
+++ src/commands/popular.js
@@ -1,73 +1,82 @@
-import CommandStructure from '../structures/command';
+import CommandStructure from "../structures/command";
 
 /**
  * Popular command.
  */
 class PopularCommand extends CommandStructure {
-    /**
-     * Create popular command.
-     * 
-     * @param {Object} client - DMDb client extends Eris
-     */
-    constructor(client) {
-        super(client, {
-            description: 'Current popular movies and TV shows on TMDb.',
-            usage: false,
-            flags: ['page', 'tv'],
-            developerOnly: false,
-            hideInHelp: false,
-            weight: 100
-        });
-    }
+  /**
+   * Create popular command.
+   *
+   * @param {Object} client - DMDb client extends Eris
+   */
+  constructor(client) {
+    super(client, {
+      description: "Current popular movies and TV shows on TMDb.",
+      usage: false,
+      flags: ["page", "tv"],
+      developerOnly: false,
+      hideInHelp: false,
+      weight: 100,
+      aliases: ["best", "top"],
+    });
+  }
 
-    /**
-     * Function to run when command is executed.
-     * 
-     * @param {Object} message - Message object
-     * @param {Array} commandArguments - Command arguments
-     * @param {Object} guildSettings - Guild settings
-     * @returns {*} A bit of everything...
-     */
-    async executeCommand(message, commandArguments, guildSettings) {
-        // Status "Searching..." message.
-        const statusMessage = await this.searchingMessage(message);
-        if (!statusMessage) return; // No permission to send messages.
+  /**
+   * Function to run when command is executed.
+   *
+   * @param {Object} message - Message object
+   * @param {Array} commandArguments - Command arguments
+   * @param {Object} guildSettings - Guild settings
+   * @returns {*} A bit of everything...
+   */
+  async executeCommand(message, commandArguments, guildSettings) {
+    // Status "Searching..." message.
+    const statusMessage = await this.searchingMessage(message);
+    if (!statusMessage) return; // No permission to send messages.
 
-        // Check for flags.
-        const flags = this.flags.parse(message.content, this.meta.flags);
-        message.content = flags.query; // Remove flags from query.
+    // Check for flags.
+    const flags = this.flags.parse(message.content, this.meta.flags);
+    message.content = flags.query; // Remove flags from query.
 
-        // Get media source.
-        const media = this.mediaSource(flags);
+    // Get media source.
+    const media = this.mediaSource(flags);
 
-        // Get options from API.
-        const options = this.APIOptions(guildSettings, { page: flags.page || message.content });
+    // Get options from API.
+    const options = this.APIOptions(guildSettings, {
+      page: flags.page || message.content,
+    });
 
-        // Get results from API.
-        const response = await this.tmdb[media].popular(options);
-        if (response.error) return this.embed.error(statusMessage, response.error);
+    // Get results from API.
+    const response = await this.tmdb[media].popular(options);
+    if (response.error) return this.embed.error(statusMessage, response.error);
 
-        // Edit status message with response.
-        this.embed.edit(statusMessage, {
-            title: `Currently Popular ${flags.tv ? 'TV Shows' : 'Movies'}`,
-            url: flags.tv ? 'https://www.themoviedb.org/tv' : 'https://www.themoviedb.org/movie',
+    // Edit status message with response.
+    this.embed.edit(statusMessage, {
+      title: `Currently Popular ${flags.tv ? "TV Shows" : "Movies"}`,
+      url: flags.tv
+        ? "https://www.themoviedb.org/tv"
+        : "https://www.themoviedb.org/movie",
 
-            thumbnail: { url: this.thumbnailURL(response.results[0].poster_path) },
-            description: this.resultsDescription(response),
+      thumbnail: { url: this.thumbnailURL(response.results[0].poster_path) },
+      description: this.resultsDescription(response),
 
-            fields: response.results.map((result) => flags.tv ? this.resultField(result.name, [
-                // Show
-                `First Air Date: ${this.date(result.first_air_date)}`,
-                `Vote Average: ${this.check(result.vote_average)}`,
-                this.TMDbID(result.id),
-            ]) : this.resultField(result.title, [
-                // Movie
-                `Release Date: ${this.date(result.release_date)}`,
-                `Vote Average: ${this.check(result.vote_average)}`,
-                this.TMDbID(result.id),
-            ])),
-        });
-    }
+      fields: response.results.map((result) =>
+        flags.tv
+          ? this.resultField(result.name, [
+              // Show
+              `First Air Date: ${this.date(result.first_air_date)}`,
+              `Vote Average: ${this.check(result.vote_average)}`,
+              this.TMDbID(result.id),
+            ])
+          : this.resultField(result.title, [
+              // Movie
+              `Release Date: ${this.date(result.release_date)}`,
+              `Vote Average: ${this.check(result.vote_average)}`,
+              this.TMDbID(result.id),
+            ])
+      ),
+    });
+  }
 }
 
 export default PopularCommand;
